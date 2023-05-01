8c4fc5ab1935f456f0765d0b3b74422621ce2f8c
Wari <waripamooweidudafa@gmail.com>
2023-05-01 14:47:14 +0100

    🐦 send_chirps_to_database

--- app/pages/Homepage.js
+++ app/pages/Homepage.js
@@ -1,36 +1,30 @@
 import React, { useState } from 'react';
 import { SafeAreaView, Button, TextInput } from 'react-native';
-import { ref, set, onValue } from "firebase/database";
+import { ref, set } from "firebase/database";
 import { db } from '../../firebaseConfig';
 
-const users = ref(db, 'users/');
-let data;
+let userid = "1/";
 
-function AddData(userId, name, email) {
-  set(ref(db, 'users/' + userId), {
-    username: name,
-    email: email,
+function AddData(chirp) {
+
+  set(ref(db, 'chirps/' + userid ), {
+    chirp: chirp,
   });
 }
 
-onValue(users, (snapshot) => {
-  data = snapshot.val();
-});
-
 function Homepage(props) {
-    const [name, setName] = useState("");
-    const [email, setEmail] = useState("");
-    return (
-        <SafeAreaView>
-            <TextInput placeholder='Name' value={name} onChangeText={setName}></TextInput>
-            <TextInput placeholder='Email' value={email} onChangeText={setEmail}></TextInput>
-            <Button title='Add data' onPress={() => {
-                AddData( data.length,  name, email )
-                setName("")
-                setEmail("")
-            }}></Button>
-        </SafeAreaView>
-    );
+
+  const [chirp, setChirp] = useState("");
+
+  return (
+    <SafeAreaView>
+      <TextInput placeholder='chirp' value={chirp} onChangeText={setChirp}></TextInput>
+      <Button title='Chirp!' onPress={() => {
+        AddData( chirp )
+        setChirp("")
+      }}></Button>
+    </SafeAreaView>
+  );
 }
 
 export default Homepage;
